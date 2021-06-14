0ad729a5f29cb46829eb40add86b5a4831633217
gabriele131099 <gabrisanna131099@gmail.com>
2021-06-14 15:10:14 +0200

    struttura

--- script.js
+++ script.js
@@ -1,25 +1,28 @@
 var arrayNum = [] 
 var num
+var arrayMemo= []
+var punti = 0
+var arrayMemoB = []
+
 for (var i = 0; i < 5; i++) {
     num = Math.floor(Math.random()*9)
     arrayNum.push(num)
 }
-setTimeout(gioca , 30000);
+setTimeout(gioca , 3000);
 
 function gioca() {
-    var arrayMemo= []
-    var punti = 0
-    var arrayMemoB = []
     for (var i = 0; i < arrayNum.length; i++) {
-        var memo = prompt("che numeri hai visto")
+        var memo = prompt("che numeri hai visto? ")
         arrayMemo.push(memo)
-        if ( memo === arrayNum[i]) {
+        if ( memo == arrayNum[i]) {
             punti += 15
             arrayMemoB.push(memo)
         } else {
+           
+
         }
     }    
-    document.getElementById("result").innerHTML = ("i tuoi punti sono" + punti + "</br> I numeri corretti sono" + arrayMemoB + "</br>" + arrayNum) 
+    document.getElementById("result").innerHTML = ("I tuoi punti sono: " + punti + "</br> I tuoi numeri  sono: " + arrayMemo + "</br> I numeri corretti sono: " + arrayMemoB + "</br>" + arrayNum) 
 }
 alert("osserva i seguenti numeri " + arrayNum)
 

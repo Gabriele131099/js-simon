9c35d0c1290abf3bbca07367294e53a1ba0e77d2
gabriele131099 <gabrisanna131099@gmail.com>
2021-06-14 14:57:31 +0200

    struttura

--- script.js
+++ script.js
@@ -13,14 +13,15 @@ function gioca() {
     for (var i = 0; i < arrayNum.length; i++) {
         var memo = prompt("che numeri hai visto")
         arrayMemo.push(memo)
-        if ( memo === arrayMemo[i]) {
+        if ( memo === arrayNum[i]) {
             punti += 15
             arrayMemoB.push(memo)
         } else {
         }
     }    
-    document.getElementById("result").innerHTML = ("i tuoi punti sono" + punti + "</br> I numeri corretti sono" + arrayMemoB)
+    document.getElementById("result").innerHTML = ("i tuoi punti sono" + punti + "</br> I numeri corretti sono" + arrayMemoB + "</br>" + arrayNum) 
 }
+alert("osserva i seguenti numeri " + arrayNum)
 
 
 
